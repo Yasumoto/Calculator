22efe3ea75624c2588665a7605335b0801060ecc
Joe Smith <yasumoto7@gmail.com>
2012-04-08 18:11:55 -0700

    Draw Axes in GraphView.
    Signed-off-by: Joe Smith <yasumoto7@gmail.com>

--- Calculator/GraphingView.m
+++ Calculator/GraphingView.m
@@ -7,25 +7,51 @@
 //
 
 #import "GraphingView.h"
+#import "AxesDrawer.h"
+
+@interface GraphingView()
+- (void) setup;
+@end
 
 @implementation GraphingView
+
+@synthesize scale = _scale;
+@synthesize dataSource = _dataSource;
+
+#define DEFAULT_SCALE 0.90
+
+- (CGFloat) scale {
+    if (!_scale) {
+        return DEFAULT_SCALE;
+    }
+    return _scale;
+}
+
+- (void) setup
+{
+}
+
+- (void) awakeFromNib
+{
+    [self setup];
+}
 
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
-        // Initialization code
+        [self setup];
     }
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
+
 - (void)drawRect:(CGRect)rect
 {
-    // Drawing code
+    CGPoint center;
+    center.x = self.bounds.origin.x + self.bounds.size.width / 2;
+    center.y = self.bounds.origin.y + self.bounds.size.height / 2;
+    [AxesDrawer drawAxesInRect:self.bounds originAtPoint:center scale:self.scale];
 }
-*/
 
 @end
