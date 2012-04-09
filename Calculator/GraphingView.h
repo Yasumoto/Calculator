ad10d51ce8f080ff084d563c07d0412dab0460ba
Joe Smith <yasumoto7@gmail.com>
2012-04-08 20:33:17 -0400

    Add new GraphingView.
    Signed-off-by: Joe Smith <yasumoto7@gmail.com>

---
+++ Calculator/GraphingView.h
@@ -0,0 +1,13 @@
+//
+//  GraphingView.h
+//  Calculator
+//
+//  Created by Joe Smith on 4/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphingView : UIView
+
+@end
---
+++ Calculator/GraphingView.m
@@ -0,0 +1,31 @@
+//
+//  GraphingView.m
+//  Calculator
+//
+//  Created by Joe Smith on 4/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "GraphingView.h"
+
+@implementation GraphingView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
